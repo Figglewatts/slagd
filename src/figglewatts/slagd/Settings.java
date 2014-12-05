8c8d8d57460fe1c16c385b849fa34ff8118df7c2
Figglewatts <figglewatts@hotmail.co.uk>
2014-12-05 18:26:22 +0000

    + Made VIRTUAL_VIEWPORT_WIDTH and HEIGHT be set to default width and height instead of null, so as to avoid errors from not setting them + Added MIN_FILTER and MAG_FILTER, to set framebuffer texture filtering

--- src/figglewatts/slagd/Settings.java
+++ src/figglewatts/slagd/Settings.java
@@ -1,8 +1,13 @@
 package figglewatts.slagd;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+
 public class Settings {
 	private Settings() { }
 	
-	public static int VIRTUAL_VIEWPORT_WIDTH;
-	public static int VIRTUAL_VIEWPORT_HEIGHT;
+	public static int VIRTUAL_VIEWPORT_WIDTH = Gdx.graphics.getWidth();
+	public static int VIRTUAL_VIEWPORT_HEIGHT = Gdx.graphics.getHeight();
+	public static TextureFilter MIN_FILTER = TextureFilter.Nearest;
+	public static TextureFilter MAG_FILTER = TextureFilter.Nearest;
 }
